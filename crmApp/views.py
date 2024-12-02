062a6c1981ddecb34afced052d5289fcd92371a1
@Mikist <mikeochango3@gmail.com>
2024-12-02 19:28:45 +0300

    create register form and implement it

--- crmApp/views.py
+++ crmApp/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
 def home(request):
 
     return render(request, 'crmApp/index.html')
+
+def register(request):
+     form = CreateUserForm()
+
+     if request.method == 'POST':
+          
+          form = CreateUserForm()
+
+          if form.is_valid():
+                
+                form.save()
+
+     context = {'form':form}
+
+     return render(request, 'crmApp/register.html', context)
