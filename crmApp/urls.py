f9b27ac5712dfeb7885e9f17aecba87409824da5
@Mikist <mikeochango3@gmail.com>
2024-12-03 12:28:43 +0300

    Enable user to create account

--- crmApp/urls.py
+++ crmApp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import home, register,login
+from .views import home, register,login,dashboard,logout
 
 
 urlpatterns = [
     path('', home, name=''),
+
     path('register/', register, name='register' ),
+
     path('login/', login, name='login' ),
+
+    path('logout/', logout, name='logout' ),
+
+    path('dashboard/', dashboard, name='dashboard' ),
+
 ]
